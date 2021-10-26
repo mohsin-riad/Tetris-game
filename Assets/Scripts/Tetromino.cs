e8de38e6419905bb40b387bbd1eb279cb879a496
mohsin <morteen1997@gmail.com>
2021-10-26 15:38:08 +0600

    Scripts updated

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using UnityEngine.Tilemaps;
 public enum Tetromino{
     I,
@@ -8,9 +9,12 @@ public enum Tetromino{
     S,
     Z,
 }
-
+[System.Serializable]
 public struct TetrominoData{
     public Tetromino tetromino;
     public Tile tile;
-
+    public Vector2Int[] cells { get; private set; }
+    public void Initialize(){
+        this.cells = Data.Cells[this.tetromino];
+    }
 }
