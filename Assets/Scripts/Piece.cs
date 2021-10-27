eeae652c5a6dd42cfafd43d54453e0cda6e38984
mohsin <morteen1997@gmail.com>
2021-10-27 19:13:06 +0600

    Wall-Kicks added

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -63,7 +63,15 @@ public class Piece : MonoBehaviour
         return valid;
     }
     public void Rotate(int direction){
+        int originalRotation = this.rotationIndex;
         this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4);
+        ApplyRotationMatrix(direction);
+        if(!TestWallKicks(this.rotationIndex, direction)){
+            this.rotationIndex = originalRotation;
+            ApplyRotationMatrix(-direction);
+        }
+    }
+    private void ApplyRotationMatrix(int direction){
         for(int i=0; i<this.cells.Length ;i++){
             Vector3 cell = this.cells[i];
             int x, y;
@@ -84,6 +92,24 @@ public class Piece : MonoBehaviour
             }
             this.cells[i] = new Vector3Int(x, y, 0); 
         }
+    }
+    private bool TestWallKicks(int rotationIndex, int rotationDirection){
+        int wallKickIndex = GetWallKickIndex(rotationIndex, rotationDirection);
+
+        for (int i = 0; i < this.data.wallKicks.GetLength(1); i++){
+            Vector2Int translation = this.data.wallKicks[wallKickIndex, i];
+            if (Move(translation)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private int GetWallKickIndex(int rotationIndex, int rotationDirection){
+        int wallKickIndex = rotationIndex * 2;
+        if(rotationDirection < 0){
+            wallKickIndex--;
+        }
+        return Wrap(wallKickIndex, 0, this.data.wallKicks.GetLength(0));
     }
     private int Wrap(int input, int min, int max)
     {
